285e79891b3a45940f3d2a245daab14b7a74c4d8
rfloarea <account@rfloarea.net>
2024-03-08 20:10:46 -0500

    Node class and clean up

--- script.js
+++ script.js
@@ -1,66 +1,88 @@
 console.log("it works!!")
 
+/* Questions 
+1. How do we know what "next" is?
+2. What actually is a "reference to the next node"?
+    - "head" seems to just be some idea that we (humans) use for the purpose of writing this code.
+
+*/
+
 // LinkedList Class
 
 class LinkedList {
-    constructor() {
+    constructor(listName) {
+        this.listName = listName;
+    }
+
+    // add a new node to the start of the list
+    prepend(value) {
+        /*
+        create newNode
+        reset head to newNode
+        */
+       head = new Node("A", "head");
+    }
+
+    // add a new node to the end of the list
+    append(value) {
+    
+    }
+
+    // returns the first node in the list
+    head() {
+    
+    }
+
+    // removes the last element from the list
+    pop() {
+    
+    }
+
+    // returns the last node in the list
+    tail() {
+    
+    }
+
+    // returns the node at the given index
+    at(index) {
+    
+    }
+
+    // returns the total number of nodes in the list
+    size() {
         
     }
 
-    append(value) {
-        // adds a new node containing value to the end of the list
-    }
-
-    prepend(value) {
-        // adds a new node containing value to the start of the list
-    }
-
-    pop() {
-        // removes the last element from the list
-    }
-
-    head() {
-        // returns the first node in the list
-    }
-
-    tail() {
-        // returns the last node in the list
-    }
-
-    at(index) {
-        // returns the node at the given index
-    }
-
-    size() {
-        // returns the total number of nodes in the list
-    }
-
+    // returns true if the passed in value is in the list, otherwise returns false
     contains(value) {
-        // returns true if the passed in value is in the list and otherwise returns false.
+    
     }
 
+    // returns the index of the node containing value, or null if not found
     find(value) {
-        // returns the index of the node containing value, or null if not found.
+
     }
 
+    // inserts a new node with the provided value at the given index
     insertAtValue(value, index) {
-        // that inserts a new node with the provided value at the given index.
+    
     }
 
+    // removes the node at the given index
     removeAt(index) {
-        // that removes the node at the given index.
+    
     }
 
+    // represents the LinkedList objects as strings, so we can print them out and preview them in the console. The format should be: `( value ) -> ( value ) -> ( value ) -> null`
     toString() {
-        // represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: `( value ) -> ( value ) -> ( value ) -> null`
     }
 }
 
 // Node Class
 
 class Node {
-    constructor(data, next) {
+    constructor(data) {
         this.data = data;
-        this.next = next;
+        this.next = null;
     }
 }
